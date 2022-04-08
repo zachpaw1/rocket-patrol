61286d9c58d06ef0b0d0e2dde0614a43dac5e9c7
zachpaw1 <zac.2019@gmail.com>
2022-04-07 20:46:19 -0700

    Updated on last commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,12 +4,12 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        //green UI background
+        // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
-        //white borders
+        // white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFF.setOrigin(0, 0));
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFF).setOrigin(0, 0);
     }
 }
