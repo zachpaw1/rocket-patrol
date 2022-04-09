7744e277f034f1e5972866c92689a31e1b60c3fd
zachpaw1 <zac.2019@gmail.com>
2022-04-09 15:57:25 -0700

    It's not working now

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -3,17 +3,17 @@ class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
 
-        // add object to existing scene
         scene.add.existing(this);       // add to existing, displayList, updateList
         this.isFiring = false;          // track rocket's firing status
         this.moveSpeed = 2;             //pixels per frame
     }
+
     update() {
         // left and right movement
         if(!this.isFiring) {
             if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
                 this.x -= this.moveSpeed;
-            } else if (keyRight.isDown && this.x <= game.config.width - borderUISize - this.width) {
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
                 this.x += this.moveSpeed;
             }
         }
