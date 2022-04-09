2a51136765dd4ea7a855af702884fb60af8aa5b1
zachpaw1 <zac.2019@gmail.com>
2022-04-09 16:42:09 -0700

    Updated Play.js
    Done with The Rocket Class: Creating the Class section. no errors

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -25,7 +25,6 @@ class Play extends Phaser.Scene {
 
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
-     //   this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
 
         // add spaceships (x3)
        // this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0,0);
