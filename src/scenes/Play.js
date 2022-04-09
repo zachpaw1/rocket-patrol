42b7c45d0b5ef52785c2d7e56069f53ef82abb5f
zachpaw1 <zac.2019@gmail.com>
2022-04-09 16:24:54 -0700

    Fixed the background somehow
    I have no idea how, but it works again

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,9 +5,9 @@ class Play extends Phaser.Scene {
     
     preload() {
         // load images/tile sprites
-        this.load.image('rocket', 'rocket-patrol/assets/rocket.png');
-        this.load.image('spaceship', 'rocket-patrol/assets/spaceship.png');
-        this.load.image('starfield', 'rocket-patrol/assets/starfield.png');
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
       }
 
     create() {
