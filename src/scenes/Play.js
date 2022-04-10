62d10c24d760dae3b82740a6f64d675945eed4c2
zachpaw1 <zac.2019@gmail.com>
2022-04-09 20:25:35 -0700

    Game Timer and End Condition

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -63,14 +63,28 @@ class Play extends Phaser.Scene {
             fixedWidth: 100
           }
           this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+
+          // GAME OVER flag
+          this.gameOver = false;
+
+          // 60-second play clock
+          scoreConfig.fixedWidth = 0;
+          this.clock = this.time.delayedCall(1000, () => {
+              this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+              this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+              this.gameOver = true;
+            }, null, this);
     }
 
     update() {
         this.starfield.tilePositionX -= 4;
-        this.p1Rocket.update();
-        this.ship01.update();                 // update spaceships (x3)
-        this.ship02.update();
-        this.ship03.update();
+        if (!this.gameOver) {
+            this.p1Rocket.update();               // update rocket sprite
+            this.ship01.update();                 // update spaceships (x3)
+            this.ship02.update();
+            this.ship03.update();
+        }
+
         // check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
             this.p1Rocket.reset();
