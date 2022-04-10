f8796f107117a90e0ce2b29ff516b1fd1b3fbcb2
zachpaw1 <zac.2019@gmail.com>
2022-04-09 21:28:26 -0700

    Updated Menu.js
    Difficulty Settings

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -33,5 +33,32 @@ class Menu extends Phaser.Scene {
     menuConfig.backgroundColor = '#00FF00';
     menuConfig.color = '#000';
     this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+
+    // define keys
+    keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            
+            // easy mode
+            game.settings = {
+                spaceshipSpeed: 3,
+                gameTimer: 60000
+            }
+            this.sound.play('sfx_select');
+            this.scene.start('playScene');
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+
+            // hard mode
+            game.settings = {
+                spaceshipSpeed: 4,
+                gameTimer: 45000
+            }
+            this.sound.play('sfx_select');
+            this.scene.start('playScene');
+        }
     }
 }
