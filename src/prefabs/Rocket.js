bde8420c6263d47aba1e253aeb8007611b12aad9
zachpaw1 <zac.2019@gmail.com>
2022-04-09 20:59:39 -0700

    Sound Part 2
    Added in Rocket SFX

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -3,9 +3,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
 
-        scene.add.existing(this);       // add to existing, displayList, updateList
-        this.isFiring = false;          // track rocket's firing status
-        this.moveSpeed = 2;             //pixels per frame
+        scene.add.existing(this);                            // add to existing, displayList, updateList
+        this.isFiring = false;                               // track rocket's firing status
+        this.moveSpeed = 2;                                  //pixels per frame
+        this.sfxRocket = scene.sound.add('sfx_rocket');     // add rocket sfx
     }
 
     update() {
@@ -18,8 +19,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         // fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();      // play sfx
         }
         // if fired, move up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
